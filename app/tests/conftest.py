abfe8bd2136dabdafba52c4b91017f35af0a8ace
emile robert <emile.robert123@gmail.com>
2019-04-21 21:03:12 +0200

    asd

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -21,11 +21,11 @@
 
 
 # from app import app
-from flask import Flask
+from app import app
 import pytest 
 
 @pytest.fixture
 def client():
-  app = Flask(__name__) 
+  # app = Flask(__name__) 
   client = app.test_client()
   return client
--- app/tests/test_laneck.py
+++ app/tests/test_laneck.py
@@ -1,5 +1,7 @@
 import pytest
-from conftest import client
+# from conftest import client 
+from app import app
+
 
 def test_assert():
     assert True
@@ -7,3 +9,9 @@ def test_assert():
 def test_home_page(client):
   response = client.get('/')
   assert response.status_code == 200
+
+@pytest.fixture
+def client():
+  # app = Flask(__name__) 
+  client = app.test_client()
+  return client
