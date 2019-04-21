206c2106e162f502935092a9dc6a812da3047097
Emile Robert <emile.robert123@gmail.com>
2019-04-21 13:20:14 +0100

    'asd'

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'postgres://lqwcftopfzmggv:f3c31ef8bfc07183d91284629f7ac9ea82d2711f9c44af7672a9ed0aecb4b574@ec2-50-17-227-28.compute-1.amazonaws.com:5432/d1nq40njqg0fl'    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
