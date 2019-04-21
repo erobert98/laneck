bf03b7b3fb99d06d9822742497fc6202d5e9c11a
emile robert <emile.robert123@gmail.com>
2019-04-21 17:46:06 +0200

    asd

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
-from config import Config
+from config import Config, Testing
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_sslify import SSLify
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
 Bootstrap(app)
 # sslify = SSLify(app)
-app.config.from_object(Config)
+app.config.from_object(Testing)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
