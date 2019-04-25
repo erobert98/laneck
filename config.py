c5fcf72846564a7d287813650be1e2a462b7522c
emile robert <emile.robert123@gmail.com>
2019-04-25 18:52:16 +0200

    revert to 742b3a89bc0b36690739820f7a1c34d71f3f69e8'

--- config.py
+++ config.py
@@ -7,8 +7,9 @@ class Config(object):
     FLASK_ADMIN_SWATCH = 'cerulean'
     ALLOWED_EXTENSIONS = set(['mp3','wav'])
     S3_BUCKET                 = 'laneck'
-    S3_KEY                    = os.environ.get("S3_ACCESS_KEY")
-    S3_SECRET                 = os.environ.get("S3_SECRET_ACCESS_KEY")
+    S3_KEY                    = 'AKIAJRUHIPGPLFV56AVQ'
+    S3_SECRET                 = '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'
+    S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 
 class Testing(object):
@@ -19,6 +20,7 @@ class Testing(object):
     FLASK_ADMIN_SWATCH = 'cerulean'
     ALLOWED_EXTENSIONS = set(['mp3', 'wav'])
     PRESERVE_CONTEXT_ON_EXCEPTION  = True
-    S3_BUCKET  = 'laneck'
-    S3_KEY = os.environ.get("S3_ACCESS_KEY")
-    S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+    S3_BUCKET                 = 'laneck'
+    S3_KEY                    = 'AKIAJRUHIPGPLFV56AVQ'
+    S3_SECRET                 = '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'
+    S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
